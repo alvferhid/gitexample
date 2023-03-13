2dcd0f76e66bfe41b29f1a1893c0b529c6c847b9
alvaro <alvferhid@gmail.com>
2023-03-13 19:38:44 +0100

    Add new greeting messages

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("patata");
+        System.out.println("Cómo estás?");
     }
 }
