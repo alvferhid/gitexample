7b0c11f767935e3ac8c119b708227b5c104831b4
alvaro <alvferhid@gmail.com>
2023-03-15 18:35:41 +0100

    Nueva clase Customer

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola");
-        System.out.println("patata");
-        System.out.println("Cómo estás?");
+
     }
 }
